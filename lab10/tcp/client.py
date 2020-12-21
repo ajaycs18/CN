6e1860696f981bf04648ec760a2fbdbb39e6d4e1
Ajay Mittur <ajay.cs18@bmsce.ac.in>
2020-12-21 14:22:05 +0530

    Add files via upload

---
+++ lab10/tcp/client.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+print('------------------ Client ------------------') 
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    while True:
+        filename = input('Enter file to request from server: ')
+
+        if not filename: 
+            break
+
+        sock.sendall(bytes(filename, 'utf-8'))
+        print(f'Sent: {filename}')
+
+        data = sock.recv(1024)
+        contents = data.decode('utf-8')
+        print(f'Received: {contents}')
+        print()
+
